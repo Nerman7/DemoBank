22f7178d3c3ffde0cacf38a256e7865bf69fa5c1
Nerman7 <nermankovacevic@live.com>
2022-10-02 10:51:19 +0200

    change button on empty fields, error messages

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect, useRef  } from "react";
+import { useState, useEffect, useRef } from "react";
 import leonus from "../../assets/resources/img_leonus_logo.svg";
 import "./login.css";
 import Button from "../../components/login/Button";
@@ -8,64 +8,29 @@ import "../../assets/fonts/axiforma/axiforma-bold.otf";
 import "../../assets/fonts/axiforma/axiforma-regular.otf";
 
 export default function Login() {
-  const [mail, setMail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const [disabled, setDisabled] = useState(true)
-  
-  const firstRender = useRef(true)
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const firstRender = useRef(true);
+  const [nameError, setNameError] = useState(null);
 
   useEffect(() => {
     if (firstRender.current) {
-      firstRender.current = false  // it's no longer the first render
-      return // skip the code below
+      firstRender.current = false;
+      return;
     }
-    setDisabled(formValidation())
-  }, [mail, password]) 
-
-
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
-
-  // function handleChange(e) {
-  //   const { name, value } = e.target;
-  //   setFormValues({ ...formValues, [name]: value });
-  //   console.log(formValues);
-  // }
-  function handleSubmit(e) {
-    // e.preventDefault();
-    // setFormErrors(validate(formValues));
-    // setIsSubmit(true);
-  }
+    setDisabled(formValidation());
+  }, [mail, password]);
 
   const formValidation = () => {
     if (mail === "" || password === "") {
-      return true
+      setNameError("Mail or password cant be blank!");
+      return true;
     } else {
-      return false
+      setNameError(null);
+      return false;
     }
-  }
-
-  // function validate() {
-  //   const errors = {};
-  //   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-
-  //   if (!values.mail) {
-  //     errors.mail = "Email is required";
-  //   } else if (!regex.test(values.mail)) {
-  //     errors.mail = "This is not a valid format. Please try again.";
-  //   }
-
-  //   if (!values.password) {
-  //     errors.password = "Password is required";
-  //   } else if (values.password.length < 4) {
-  //     errors.password = "Password is too short.";
-  //   } else if (values.password.length > 10) {
-  //     errors.password = "Password is too long.";
-  //   }
-
-  //   return errors;
-  // }
+  };
 
   return (
     <div className="loginPage">
@@ -73,36 +38,37 @@ export default function Login() {
         <source src={bacgroundVideo} type="video/mp4"></source>
       </video>
       <div className="centerPage">
-        {(Object.keys(formErrors).length === 0) & isSubmit ? (
+        {/* {(mail && password) ? (
           <h1>Signed in!</h1>
         ) : (
           <div></div>
-        )}
+        )}  */}
         <img className="logoImg" src={leonus} alt="Please wait"></img>
         <h1 className="mainTitle">Welcome</h1>
         <h3 className="credentials">Please enter your credentials to login.</h3>
-        <form className="loginPassAndMail" onSubmit={handleSubmit}>
+        <form className="loginPassAndMail">
           <input
             className="loginInputs"
-            onChange={ e => setMail(e.target.value) }
+            onChange={(e) => setMail(e.target.value)}
             value={mail}
             placeholder="Your e-mail adress"
             type="text"
             name="mail"
             required
           ></input>
-          <p>{formErrors.mail}</p>
+          {nameError && <p>{nameError}</p>}
           <input
             className="loginInputs"
-            onChange={ e => setPassword(e.target.value) }
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Your password"
             type="password"
             value={password}
             name="password"
             required
           ></input>
-          <p>{formErrors.password}</p>
-          <button type="submit" disabled={disabled} >Login</button>
+          <Button type="submit" disabled={disabled}>
+            Login
+          </Button>
         </form>
       </div>
     </div>
--- src/components/login/Button.js
+++ src/components/login/Button.js
@@ -2,12 +2,12 @@ import React from "react";
 import arrow from "../../assets/resources/ic_next.svg";
 import "./button.css";
 
-export default function Button() {
+export default function Button({disabled, type}) {
 
   return (
     <div>
-        <button className="btnLogin" >
-          Login <img className="btnArrow" src={arrow} alt="da" type='button'></img>
+        <button className={`${disabled ? 'btnLoginOpa': 'btnLogin'}`}  type={type} disabled={disabled}>
+          Login <img className="btnArrow" src={arrow} alt="da" ></img>
         </button>
     </div>
   );
