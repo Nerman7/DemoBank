38fd2c5078104c2f487d02c85e756976fb827a53
Nerman7 <nermankovacevic@live.com>
2022-10-04 15:16:10 +0200

    localization finish

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,8 +6,10 @@ import Button from "../../components/login/Button";
 import bacgroundVideo from "../../assets/resources/login_video.mp4";
 import "../../assets/fonts/axiforma/axiforma-bold.otf";
 import "../../assets/fonts/axiforma/axiforma-regular.otf";
+import { useTranslation } from "react-i18next";
 
 export default function Login() {
+  const { t } = useTranslation();
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [disabled, setDisabled] = useState(true);
@@ -44,30 +46,30 @@ export default function Login() {
           <div></div>
         )}  */}
         <img className="logoImg" src={leonus} alt="Please wait"></img>
-        <h1 className="mainTitle">Welcome</h1>
-        <h3 className="credentials">Please enter your credentials to login.</h3>
+        <h1 className="mainTitle">{t("welcome")}</h1>
+        <h3 className="credentials">{t("credentials")}</h3>
         <form className="loginPassAndMail">
           <input
             className="loginInputs"
             onChange={(e) => setMail(e.target.value)}
             value={mail}
-            placeholder="Your e-mail adress"
+            placeholder={t("email")}
             type="text"
             name="mail"
             required
           ></input>
-          {nameError && <p>{nameError}</p>}
+          {nameError && <p>{t("error")}</p>}
           <input
             className="loginInputs"
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="Your password"
+            placeholder={t("password")}
             type="password"
             value={password}
             name="password"
             required
           ></input>
           <Button type="submit" disabled={disabled}>
-            Login
+            {t("button")}
           </Button>
         </form>
       </div>
--- src/components/login/Button.js
+++ src/components/login/Button.js
@@ -1,14 +1,20 @@
 import React from "react";
 import arrow from "../../assets/resources/ic_next.svg";
 import "./button.css";
+import { useTranslation } from "react-i18next";
 
-export default function Button({disabled, type}) {
-
+export default function Button({ disabled, type }) {
+  const { t } = useTranslation();
   return (
     <div>
-        <button className={`${disabled ? 'btnLoginOpa': 'btnLogin'}`}  type={type} disabled={disabled}>
-          Login <img className="btnArrow" src={arrow} alt="da" ></img>
-        </button>
+      <button
+        className={`${disabled ? "btnLoginOpa" : "btnLogin"}`}
+        type={type}
+        disabled={disabled}
+      >
+        {t("button")}
+        <img className="btnArrow" src={arrow} alt="da"></img>
+      </button>
     </div>
   );
 }
