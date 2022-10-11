8d74d88974a540e5acffabfa7cea43687a6f5624
Nerman7 <nermankovacevic@live.com>
2022-10-11 17:53:28 +0200

    change variable names

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,83 +1,87 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
-import leonus from "../../assets/resources/img_leonus_logo.svg";
+import login_leonus_logo from "../../assets/resources/img_leonus_logo.svg";
 import "./login.css";
 import Button from "../../components/login/Button";
-import bacgroundVideo from "../../assets/resources/login_video.mp4";
+import login_bacground_video from "../../assets/resources/login_video.mp4";
 import "../../assets/fonts/axiforma/axiforma-bold.otf";
 import "../../assets/fonts/axiforma/axiforma-regular.otf";
 import { useTranslation } from "react-i18next";
 
 export default function Login() {
   const { t } = useTranslation();
-  const [mail, setMail] = useState("");
-  const [password, setPassword] = useState("");
-  const [disabled, setDisabled] = useState(true);
-  const firstRender = useRef(true);
-  const [nameError, setNameError] = useState(null);
-  const [word, setWord] = useState('Prijavi se');
-
+  const [login_user_code, set_login_user_code] = useState("");
+  const [login_OTP, set_login_OTP] = useState("");
+  const [disabled, set_disabled] = useState(true);
+  const login_first_render = useRef(true);
+  const [login_error_warning, set_login_error_warning] = useState(null);
 
   useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
+    if (login_first_render.current) {
+      login_first_render.current = false;
       return;
     }
-    setDisabled(formValidation());
-  }, [mail, password]);
+    set_disabled(login_form_validation());
+  }, [login_user_code, login_OTP]);
 
-  const formValidation = () => {
-    if (mail === "" || password === "") {
-      setNameError("Mail or password cant be blank!");
+  const login_form_validation = () => {
+    if (login_user_code === "" || login_OTP === "") {
+      set_login_error_warning(t("login_error"));
       return true;
     } else {
-      setNameError(null);
+      set_login_error_warning(null);
       return false;
     }
   };
- 
+
   return (
-    <div className="loginPage">
-      <video autoPlay loop muted playsInline width={100} className="myVideo">
-        <source src={bacgroundVideo} type="video/mp4"></source>
+    <div className="login_main_page">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        width={100}
+        className="login_background_video"
+      >
+        <source src={login_bacground_video} type="video/mp4"></source>
       </video>
-      <div className="centerPage">
-        {/* {(mail && password) ? (
-          <h1>Signed in!</h1>
-        ) : (
-          <div></div>
-        )}  */}
-        <img className="logoImg" src={leonus} alt="Please wait"></img>
-        <h1 className="mainTitle">{t("welcome")}</h1>
-        <h3 className="credentials">{t("credentials")}</h3>
-        <form className="loginPassAndMail">
+      <div className="login_center_page">
+        <img className="login_logo_image" src={login_leonus_logo} alt="Please wait"></img>
+        <h1 className="login_main_title">{t("login_welcome_title")}</h1>
+        <h3 className="login_credentials">{t("login_credentials_text")}</h3>
+        <form className="login_mail_and_password">
           <input
-            className="loginInputs"
-            onChange={(e) => setMail(e.target.value)}
-            value={mail}
-            placeholder={t("email")}
+            className="login_mail_and_password_inputs"
+            onChange={(e) => set_login_user_code(e.target.value)}
+            value={login_user_code}
+            placeholder={t("login_email_placeholder_text")}
             type="text"
-            name="mail"
+            name="login_user_code"
             required
           ></input>
-          <div className="divErrorMessage"> {nameError && <p>{t("error")}</p>}</div>
+          <div className="login_error_message">
+            {" "}
+            {login_error_warning && <p>{t("login_error_text")}</p>}
+          </div>
           <input
-            className="loginInputs"
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder={t("password")}
+            className="login_mail_and_password_inputs"
+            onChange={(e) => set_login_OTP(e.target.value)}
+            placeholder={t("login_password_placeholder_text")}
             type="password"
-            value={password}
-            name="password"
+            value={login_OTP}
+            name="login_OTP"
             required
           ></input>
 
-          <Button 
-          textButton={word}
-          buttonColor=''
-          buttonArow={true}
-          type="submit" 
-          disabled={disabled}>
-          </Button>
+          <Button
+            text_button={t("login_button_title")}
+            button_color=''
+            button_opacity={`${disabled ? 0.5 : 1}`}
+            button_arow={true}
+            type="submit"
+            disabled={disabled}
+          ></Button>
         </form>
       </div>
     </div>
--- src/components/login/Button.js
+++ src/components/login/Button.js
@@ -3,19 +3,19 @@ import arrow from "../../assets/resources/ic_next.svg";
 import "./button.css";
 import { useTranslation } from "react-i18next";
 
-export default function Button({ disabled, type, textButton, buttonColor, buttonArow }) {
+export default function Button({ disabled, type, text_button, button_color, button_arow, button_opacity }) {
   const { t } = useTranslation();
   
   return (
     <div>
       <button
-        className={`${disabled ? "btnLoginOpa" : "btnLogin"}`}
+        className={"btnLogin"}
         type={type}
         disabled={disabled}
-        style={{backgroundColor: `${buttonColor}`}}
+        style={{backgroundColor: `${button_color}` , opacity:`${button_opacity}` }}
       >
-        {textButton}
-       { buttonArow && <img className="btnArrow" src={arrow} alt="da"></img>}
+        {text_button}
+       { button_arow && <img className="btnArrow" src={arrow} alt="da"></img>}
       </button>
     </div>
   );
